00deaa7aa08fdd3474cac1190ad22c939e36d303
Mujtaba Idrees <mujtabaidrees@gmail.com>
2025-01-05 06:42:23 +0100

    feat: add npm release github workflow

---
+++ release.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    branches: ['main'],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/npm',
+      [
+        '@semantic-release/exec', {
+          'prepareCmd': 'npm run build'
+        }      
+      ],
+      '@semantic-release/git',
+      '@semantic-release/github'
+    ],
+    git: {
+      assets: ['package.json'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    }
+  };
